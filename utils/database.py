5bf2233e7083ab5e6a964c6a51c4cb8cb306ac8b
Rashad Sirajudeen <rashad@wso2.com>
2025-07-14 09:54:24 +0530

    openapi test

--- utils/database.py
+++ utils/database.py
@@ -10,8 +10,9 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+    pass
+    # db = SessionLocal()
+    # try:
+    #     yield db
+    # finally:
+    #     db.close()
